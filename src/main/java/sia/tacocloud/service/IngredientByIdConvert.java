31ec4800a2481460d169126f82675275f4fe927b
StanyslavIzmaylov <monsterfish2@yandex.ru>
2024-11-27 23:13:10 +0300

    start

---
+++ src/main/java/sia/tacocloud/service/IngredientByIdConvert.java
@@ -0,0 +1,28 @@
+package sia.tacocloud.service;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import sia.tacocloud.service.Ingredient.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class IngredientByIdConvert implements Converter<String,Ingredient> {
+    private Map<String,Ingredient> ingredientMap = new HashMap<>();
+    public IngredientByIdConvert(){
+        ingredientMap.put("FLTO",new Ingredient("FTLTO","Flour Tortillia", Type.WRAP));
+        ingredientMap.put("COTO",(new Ingredient("COTO","Corn Tortilla", Type.WRAP)));
+        ingredientMap.put("GRBF",(new Ingredient("GRBF","Ground Beef", Type.PROTEIN)));
+        ingredientMap.put("CARN",(new Ingredient("CARN","Carnitas", Type.PROTEIN)));
+        ingredientMap.put("TMTO",(new Ingredient("TMTO","Diced Tomatoes", Type.VEGGIES)));
+        ingredientMap.put("LETC",(new Ingredient("LETC","Lettuce", Type.VEGGIES)));
+        ingredientMap.put("CHED",(new Ingredient("CHED","Cheddar", Type.CHEESE)));
+        ingredientMap.put("JACK",(new Ingredient("JACK","Monterry", Type.CHEESE)));
+        ingredientMap.put("SLSA",( new Ingredient("SLSA","Salsa", Type.SAUCE)));
+        ingredientMap.put("SRCR",(new Ingredient("SRCR","Sour Cream", Type.SAUCE)));
+    }
+    @Override
+    public Ingredient convert(String id) {
+        return ingredientMap.get(id);
+    }
+}
